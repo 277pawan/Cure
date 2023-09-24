4478404c0209ebe1a6920f29cac926c8c0c79e86
pawan <bpawan277@gmail.com>
2023-09-24 17:53:31 +0530

    koi

--- src/Component/News/News.jsx
+++ src/Component/News/News.jsx
@@ -4,6 +4,9 @@ import "./News.css";
 function News() {
   const [data, setdata] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [count, setcounter] = useState(1);
+  const [disp, setprev] = useState(true);
+  const [next, setnext] = useState(true);
 
   useEffect(() => {
     async function fetchNews() {
@@ -20,34 +23,72 @@ function News() {
 
     fetchNews();
   }, []);
-
+  useEffect(() => {
+    if (count <= 1) {
+      setprev(false);
+    } else {
+      setprev(true);
+    }
+    if (count >= 4) {
+      setnext(false);
+    } else {
+      setnext(true);
+    }
+  }, [count]);
   return (
-    <div className="newscontainer">
-      {loading ? (
-        <div className="spinner-container">
-          <div className="spinner"></div>
-        </div>
-      ) : Array.isArray(data) && data.length > 0 ? (
-        data.map((article, index) => (
-          <div key={index} className="article">
-            <h2>{article.brand}</h2>
-            {article.category && <p>Author: {article.category}</p>}
-            <p>{article.description}</p>
-            <img src={article.images[0]} alt={article.price} />
-            <a
-              className="anchor"
-              href={article.thumbnail}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read more
-            </a>
+    <>
+      <div className="newscontainer">
+        {loading ? (
+          <div className="spinner-container">
+            <div className="spinner"></div>
           </div>
-        ))
-      ) : (
-        <p>No news articles available.</p>
-      )}
-    </div>
+        ) : Array.isArray(data) && data.length > 0 ? (
+          data.slice(count * 8 - 8, count * 8).map((article, index) => (
+            <>
+              <div key={index} className="article">
+                <h2>{article.brand}</h2>
+                {article.category && <p>Author: {article.category}</p>}
+                <p>{article.description}</p>
+                <img src={article.images[0]} alt={article.price} />
+                <a
+                  className="anchor"
+                  href={article.thumbnail}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read more.
+                </a>
+              </div>
+            </>
+          ))
+        ) : (
+          <p>No news articles available.</p>
+        )}
+      </div>
+      <div className="pagination">
+        <span>
+          <button
+            className="pagination-button"
+            style={{
+              display: disp ? "inline-block" : "none",
+            }}
+            onClick={() => setcounter(count - 1)}
+          >
+            Prev
+          </button>
+          <button className="current-page">{count}</button>
+          <button
+            className="pagination-button"
+            style={{
+              display: next ? "inline-block" : "none",
+            }}
+            onClick={() => setcounter(count + 1)}
+          >
+            Next
+          </button>
+        </span>
+      </div>
+    </>
   );
 }
 
